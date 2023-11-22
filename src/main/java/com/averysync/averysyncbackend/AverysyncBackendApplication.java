8ba906bcf2953aabd110f67108f538b350faf646
Suraj Maity <surajmaity981@gmail.com>
2023-11-22 11:31:02 +0530

    first commit

---
+++ src/main/java/com/averysync/averysyncbackend/AverysyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.averysync.averysyncbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AverysyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AverysyncBackendApplication.class, args);
+	}
+
+}
